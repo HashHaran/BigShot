9140e0ac8577e370c4dc6c205f16d1e8354b0412
rolic-hub <enyiomaeze09@gmail.com>
2022-09-25 12:26:22 +0100

    update frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 
- module.exports = {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      //'black' : '#000000',
+      'violet': '#8b5cf6',
+      'light-blue': '#60a5fa',
+      'fuchsia': '#d946ef',
+      'gray' : '#6b7280',
+      'blue': '#2563eb',
+      'red': '#ef4444'
+
+
+    },
     extend: {},
   },
   plugins: [],
-}
+};
